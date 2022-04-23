3c1e8df8aa7bdb3acc08687b89da10fff32d1b02
apapiez <alex.papiez@googlemail.com>
2022-04-23 19:51:33 +0100

    savepoint

--- Assets/Scripts/AIController.cs
+++ Assets/Scripts/AIController.cs
@@ -118,7 +118,146 @@ public class AIController : MonoBehaviour
     }
 
     // a function to decide if a card can be played which takes in a card and returns a boolean
-    
+    public bool CanPlayCard(Card card)
+    {
+        // if the mana cost of the card is greater than the faction specific mana pool, return false. Get the faction name from the card faction
+        if (card.ManaCost > GameMaster.GetFactionMana(card.Faction))
+        {
+            return false;
+        }
+
+        // if the AIPlayerSentiment is negative, return false if the card is a support card
+        if (aiPlayerSentiment == AIPlayerSentimentEnum.Negative && card.Faction == "Support")
+        {
+            return false;
+        }
+
+        // if the AIPlayerSentiment is positive, return false if the card is an attack card
+        if (aiPlayerSentiment == AIPlayerSentimentEnum.Positive && card.Faction == "Attack")
+        {
+            return false;
+        }
+
+        // if the card is a support card and the AIPlayerSentiment is neutral, return false
+        if (card.Faction == "Support" && aiPlayerSentiment == AIPlayerSentimentEnum.Neutral)
+        {
+            return false;
+        }
+
+        // if the card is an attack card and the AIPlayerSentiment is neutral, return false
+        if (card.Faction == "Attack" && aiPlayerSentiment == AIPlayerSentimentEnum.Neutral)
+        {
+            return false;
+        }
+
+        // if the card is an attack card and the AIPlayerSentiment is positive, return false
+        if (card.Faction == "Attack" && aiPlayerSentiment == AIPlayerSentimentEnum.Positive)
+        {
+            return false;
+        }
+
+        // if the card is a support card and the AIPlayerSentiment is negative, return false
+        if (card.Faction == "Support" && aiPlayerSentiment == AIPlayerSentimentEnum.Negative)
+        {
+            return false;
+        }
+
+        // if the cards faction is Military, return false if the AI does not have military support and return true if the AI does have military support and the card increases the crisis progress of the military faction
+        if (card.Faction == "Military" && aISupportObjective != AISupportObjectiveEnum.MilitarySupport && card.MilitarySupportIncrease > 0)
+        {
+            return false;
+        }
+
+        // if the cards faction is Economic, return false if the AI does not have economic support and return true if the AI does have economic support and the card increases the crisis progress of the economic faction
+        if (card.Faction == "Economic" && aISupportObjective != AISupportObjectiveEnum.EconomicSupport && card.EconomicSupportIncrease > 0)
+        {
+            return false;
+        }
+
+        // if the cards faction is People, return false if the AI does not have people support and return true if the AI does have people support and the card increases the crisis progress of the people faction
+        if (card.Faction == "People" && aISupportObjective != AISupportObjectiveEnum.PeopleSupport && card.PeopleSupportIncrease > 0)
+        {
+            return false;
+        }
+
+        // if the cards faction is Nobility, return false if the AI does not have nobility support and return true if the AI does have nobility support and the card increases the crisis progress of the nobility faction
+        if (card.Faction == "Nobility" && aISupportObjective != AISupportObjectiveEnum.NobilitySupport && card.NobilitySupportIncrease > 0)
+        {
+            return false;
+        }
+        
+    }
+
+    //a function which takes in a list of cards and returns a list of cards ordered by the cards progress increase
+    // pass this function a list of cards whose faction match the AI's support objective
+    public List<Card> OrderCardsByProgressIncrease(List<Card> cards)
+    {
+        //create a new list of cards
+        List<Card> orderedCards = new List<Card>();
+
+        //loop through the cards in the list of cards
+        for (int i = 0; i < cards.Count; i++)
+        {
+            //create a new variable to hold the index of the card with the highest progress increase
+            int highestIndex = 0;
+
+            //loop through the cards in the list of cards
+            for (int j = 0; j < cards.Count; j++)
+            {
+                //if the card at the current index has a higher progress increase than the card at the highest index, set the highest index to the current index
+                if (cards[j].ProgressIncrease > cards[highestIndex].ProgressIncrease)
+                {
+                    highestIndex = j;
+                }
+            }
+
+            //add the card at the highest index to the ordered cards list
+            orderedCards.Add(cards[highestIndex]);
+
+            //remove the card at the highest index from the cards list
+            cards.RemoveAt(highestIndex);
+        }
+
+        //return the ordered cards list
+        return orderedCards;
+    }
+
+    //a function which takes in a list of cards and returns a list of cards ordered by the cards faction power increase
+    // pass this function a list of cards whose faction match the AI's support objective
+    public List<Card> OrderCardsByFactionPowerIncrease(List<Card> cards)
+    {
+        //create a new list of cards
+        List<Card> orderedCards = new List<Card>();
+
+        //loop through the cards in the list of cards
+        for (int i = 0; i < cards.Count; i++)
+        {
+            //create a new variable to hold the index of the card with the highest faction power increase
+            int highestIndex = 0;
+
+            //loop through the cards in the list of cards
+            for (int j = 0; j < cards.Count; j++)
+            {
+                //if the card at the current index has a higher faction power increase than the card at the highest index, set the highest index to the current index
+                if (cards[j].FactionPowerIncrease > cards[highestIndex].FactionPowerIncrease)
+                {
+                    highestIndex = j;
+                }
+            }
+
+            //add the card at the highest index to the ordered cards list
+            orderedCards.Add(cards[highestIndex]);
+
+            //remove the card at the highest index from the cards list
+            cards.RemoveAt(highestIndex);
+        }
+
+        //return the ordered cards list
+        return orderedCards;
+    }
+
+
+
 
 
     
